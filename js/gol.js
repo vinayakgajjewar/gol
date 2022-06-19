561399a369101f2f6e7729b9d1e0c824f1663bc4
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2022-06-19 14:33:25 -0700

    render empty board

--- js/gol.js
+++ js/gol.js
@@ -2,6 +2,10 @@ function draw() {
   const canvas = document.getElementById('gol-canvas');
   if (canvas.getContext) {
     const context = canvas.getContext('2d');
-    context.fillRect(10, 10, 10, 10);
+    for (let i = 0; i < 50; i++) {
+      for (let j = 0; j < 50; j++) {
+        context.strokeRect(i * 10, j * 10, 10, 10);
+      }
+    }
   }
 }
