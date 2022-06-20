9e7e1b315619ee3a934d6ed382519adae7047b69
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2022-06-19 21:46:54 -0700

    add utility function to clear canvas

--- js/gol.js
+++ js/gol.js
@@ -1,3 +1,9 @@
+// utility function to clear the canvas
+function clearCanvas(canvas) {
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// called when page is loaded
 function draw() {
   const canvas = document.getElementById('gol-canvas');
   if (canvas.getContext) {
